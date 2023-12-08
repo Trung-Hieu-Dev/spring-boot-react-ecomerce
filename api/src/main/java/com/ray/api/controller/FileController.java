77f95261b0fcea7aeb8ef33deb765c1ed87d2a92
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-08 12:14:50 +0700

    API: Upload and Display Image

---
+++ api/src/main/java/com/ray/api/controller/FileController.java
@@ -0,0 +1,46 @@
+package com.ray.api.controller;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/api/file")
+@CrossOrigin("http://localhost:3000")
+public class FileController {
+    
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        String message;
+        try {
+            Files.copy(file.getInputStream(), Paths.get("upload/image").resolve(file.getOriginalFilename()));
+            message = "Upload file successfully " + file.getOriginalFilename();
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        } catch (Exception e) {
+            message = "Could not upload file " + file.getOriginalFilename();
+            return new ResponseEntity<>(message, HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+    
+    @GetMapping(value = "/image/{fileName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    @ResponseBody
+    public ResponseEntity<byte[]> getFile(@PathVariable String fileName) throws IOException {
+        Path file = Paths.get("upload/image").resolve(fileName);
+        Resource resource = new UrlResource(file.toUri());
+        if (resource.exists() && resource.isReadable()) {
+            return new ResponseEntity<>(FileUtils.readFileToByteArray(resource.getFile()), HttpStatus.OK);
+        } else {
+            throw new RuntimeException("Could not find image");
+        }
+    }
+}
