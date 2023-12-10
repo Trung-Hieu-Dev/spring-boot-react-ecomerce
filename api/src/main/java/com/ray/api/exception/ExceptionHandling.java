fc0b4e1b5edf738932dfa6e8f33d091fa1db380a
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-10 21:30:21 +0700

    API: Testing Error 500

--- api/src/main/java/com/ray/api/exception/ExceptionHandling.java
+++ api/src/main/java/com/ray/api/exception/ExceptionHandling.java
@@ -9,14 +9,27 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class ExceptionHandling {
+    // Handle 404 error
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<HttpResponse> noHandlerFoundException() {
+        return createHttpResponse(HttpStatus.NOT_FOUND, "There is no mapping for this URL");
+    }
+    
+    // Handle 500 error
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<HttpResponse> internalServerErrorException(Exception ex) {
+//        return createHttpResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+//    }
+    
+    private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message) {
         return new ResponseEntity<>(
-                new HttpResponse(HttpStatus.NOT_FOUND.value(),
-                                 HttpStatus.NOT_FOUND,
-                                 HttpStatus.NOT_FOUND.getReasonPhrase(),
-                                 "There is no mapping for this URL"),
-                HttpStatus.NOT_FOUND
+            new HttpResponse(
+               httpStatus.value(),
+               httpStatus,
+               httpStatus.getReasonPhrase(),
+               message
+            ),
+            httpStatus
         );
     }
 }
