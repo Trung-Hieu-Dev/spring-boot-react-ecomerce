0dc2cdb765937cd9ed4887b2498be4877c0c78e3
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-13 17:44:13 +0700

    CLIENT: Basket Summary

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import {
+    Grid,
     Paper,
     Table,
     TableBody,
@@ -13,6 +14,7 @@ import { AddCircle, Delete, RemoveCircle } from "@mui/icons-material";
 import { StoreContext } from "../../context/StoreContext";
 import { LoadingButton } from "@mui/lab";
 import axios from "axios";
+import BasketSummary from "./BasketSummary";
 
 const BasketPage = () => {
     const {basket, setBasket, removeItem} = useContext(StoreContext);
@@ -47,61 +49,70 @@ const BasketPage = () => {
     if (!basket) return <Typography variant='h3'>No basket found..</Typography>
 
     return (
-        <TableContainer component={Paper} elevation={3}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Product</TableCell>
-                        <TableCell align="right">Price</TableCell>
-                        <TableCell align="center">Quantity</TableCell>
-                        <TableCell align="right">Subtotal</TableCell>
-                        <TableCell align="right">Actions</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {basket.basketItems.map((row) => (
-                        <TableRow
-                            key={row.productId}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">${row.unitPrice.toFixed(2)}</TableCell>
-                            <TableCell align="center">
-                                <LoadingButton
-                                    loading={status.loading && status.name === 'add' && status.id === row.productId}
-                                    color='secondary'
-                                    sx={{minWidth: 0}}
-                                    onClick={() => handleAddItem(row.productId, 'add')}
-                                >
-                                    <AddCircle />
-                                </LoadingButton>
-                                {row.quantity}
-                                <LoadingButton
-                                    loading={status.loading && status.name === 'subtract'  && status.id === row.productId}
-                                    color='error'
-                                    sx={{minWidth: 0}}
-                                    onClick={() => handleRemoveItem(row.productId, 1, 'subtract')}
-                                >
-                                    <RemoveCircle />
-                                </LoadingButton>
-                            </TableCell>
-                            <TableCell align="right">${(row.unitPrice * row.quantity).toFixed(2)}</TableCell>
-                            <TableCell align="right">
-                                <LoadingButton
-                                    loading={status.loading && status.name === 'remove' && status.id === row.productId}
-                                    color={"warning"}
-                                    onClick={() => handleRemoveItem(row.productId, row.quantity, 'remove')}
-                                >
-                                    <Delete />
-                                </LoadingButton>
-                            </TableCell>
+        <>
+            <TableContainer component={Paper} elevation={3}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Product</TableCell>
+                            <TableCell align="right">Price</TableCell>
+                            <TableCell align="center">Quantity</TableCell>
+                            <TableCell align="right">Subtotal</TableCell>
+                            <TableCell align="right">Actions</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {basket.basketItems.map((row) => (
+                            <TableRow
+                                key={row.productId}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">${row.unitPrice.toFixed(2)}</TableCell>
+                                <TableCell align="center">
+                                    <LoadingButton
+                                        loading={status.loading && status.name === 'add' && status.id === row.productId}
+                                        color='secondary'
+                                        sx={{minWidth: 0}}
+                                        onClick={() => handleAddItem(row.productId, 'add')}
+                                    >
+                                        <AddCircle />
+                                    </LoadingButton>
+                                    {row.quantity}
+                                    <LoadingButton
+                                        loading={status.loading && status.name === 'subtract'  && status.id === row.productId}
+                                        color='error'
+                                        sx={{minWidth: 0}}
+                                        onClick={() => handleRemoveItem(row.productId, 1, 'subtract')}
+                                    >
+                                        <RemoveCircle />
+                                    </LoadingButton>
+                                </TableCell>
+                                <TableCell align="right">${(row.unitPrice * row.quantity).toFixed(2)}</TableCell>
+                                <TableCell align="right">
+                                    <LoadingButton
+                                        loading={status.loading && status.name === 'remove' && status.id === row.productId}
+                                        color={"warning"}
+                                        onClick={() => handleRemoveItem(row.productId, row.quantity, 'remove')}
+                                    >
+                                        <Delete />
+                                    </LoadingButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            <Grid container mt={2}>
+                <Grid item xs={6}></Grid>
+                <Grid item xs={6}>
+                    <BasketSummary />
+                </Grid>
+            </Grid>
+        </>
     );
 };
 
